4f05244b63765e1dd517093712e16c1a5169541b
Marco Faustinelli (muzietto) <marco_faustinelli@yahoo.com>
2018-02-02 17:52:03 +0100

    first draft first webworker

--- 01_high_prime/js/highPrime.js
+++ 01_high_prime/js/highPrime.js
@@ -1,1 +1,11 @@
-// highPrime.js
+var n = 1;
+search: while (true) {
+    n += 1;
+    for (var i = 2; i < Math.sqrt(n); i += 1) {
+        if (n % i === 0) {
+            continue search;
+        }
+    }
+    postMessage(n);
+}
+
